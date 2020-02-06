5bde424aa408b2f7db188dc40c5b7005a8cd94a5
Christian Mejia <chrismejia10@gmail.com>
2020-02-05 19:13:14 -0500

    Create schema file; add schema file to server; functioning graphiql

--- Section03/server.js
+++ Section03/server.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
 const port = 1337;
 
 /**
- * If the server sees a call for a route to graphql,
- * tell it to use the express-graphql lib
+ * For the middleware we registered, "/graphql", we pass in the schema file and the graphiql object into the expressGraphQL function.
  */
-app.use(
-  "/graphql",
-  expressGraphQL({
-    graphiql: true
-  })
-);
+app.use("/graphql", expressGraphQL({ schema, graphiql: true }));
 
 app.listen(port, () => {
   console.log(`Now listening on ${port}`);
